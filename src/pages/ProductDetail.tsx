15d356cd5c67f22cfcc1f1dc302d468f7a27de7d
autsada <autsada.t@gmail.com>
2020-11-05 13:12:37 +0700

    S2 L13 Product Detail 2

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
+import PageNotFound from './PageNotFound'
 import { Product, products } from '../data/products'
 
 interface Props {}
@@ -17,8 +18,48 @@ const ProductDetail: React.FC<Props> = () => {
     else setProduct(undefined)
   }, [params])
 
-  console.log(product)
-  return <div>ProductDetail</div>
+  if (!product) return <PageNotFound />
+
+  return (
+    <div className='page--product-detail'>
+      <div className='product-detail__section'>
+        <img
+          src={product.imageUrl}
+          alt={product.title}
+          className='product-image'
+        />
+      </div>
+
+      <div className='product-detail__section'>
+        <div className='product-detail__sub-section'>
+          <h3 className='header'>{product.title}</h3>
+          <p className='paragraph'>{product.description}</p>
+        </div>
+        <div className='product-detail__sub-section'>
+          <p className='paragraph'>
+            Price:{' '}
+            <span className='paragraph--orange'>
+              ${product.price.toFixed(2)}
+            </span>
+          </p>
+        </div>
+        <div className='product-detail__sub-section product-detail__sub-section--stock'>
+          <p className='paragraph'>
+            Availability: <span className='paragraph--success'>In stock</span>
+          </p>
+        </div>
+        <div className='product-detail__sub-section quantity-control'>
+          <div className='qty-action'>-</div>
+          <div className='qty-action qty-action--qty'>
+            <p className='paragraph'>1</p>
+          </div>
+          <div className='qty-action'>+</div>
+        </div>
+
+        <button>Add to Cart</button>
+      </div>
+    </div>
+  )
 }
 
 export default ProductDetail
