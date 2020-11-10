54e05ad906d9b782d24e24e0d9b3da5c3a10dca8
autsada <autsada.t@gmail.com>
2020-11-10 17:39:53 +0700

    S3 L24 First cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,43 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp()
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
+const env = functions.config()
+
+export const onSignup = functions.https.onCall(async (data, context) => {
+  try {
+    const { username } = data as { username: string }
+
+    if (!context.auth?.uid) return
+
+    // 1. Create a role on the user in the firebase authentication
+    await admin.auth().setCustomUserClaims(context.auth.uid, {
+      role:
+        context.auth.token.email === env.admin.super_admin
+          ? 'SUPER_ADMIN'
+          : 'CLIENT',
+    })
+
+    // 2. Create a new user document in the users collection in firestore
+    const result = await admin
+      .firestore()
+      .collection('users')
+      .doc(context.auth?.uid)
+      .set({
+        username,
+        email: context.auth.token.email,
+        role:
+          context.auth.token.email === env.admin.super_admin
+            ? 'SUPER_ADMIN'
+            : 'CLIENT',
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      })
+
+    if (!result) return
+
+    return { message: 'User has been created on firestore.' }
+  } catch (error) {
+    throw error
+  }
 })
