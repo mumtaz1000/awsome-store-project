c2632f06f40b099f80e2dded821b90767091172a
autsada <autsada.t@gmail.com>
2020-11-17 14:39:39 +0700

    S4 L45 Private route 2

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 
+import Spinner from '../components/Spinner'
 import { useAuthContext } from '../state/auth-context'
 
 interface Props {}
 
 const PrivateRoute: React.FC<Props> = ({ children }) => {
+  const [authChecked, setAuthChecked] = useState(false)
   const {
     authState: { authUser },
   } = useAuthContext()
 
-  if (!authUser) return <Redirect to='/' />
+  useEffect(() => {
+    const checkAuth = setTimeout(() => {
+      setAuthChecked(true)
+    }, 2000)
+
+    return () => clearTimeout(checkAuth)
+  }, [authUser])
+
+  if (!authChecked && !authUser)
+    return <Spinner color='grey' height={50} width={50} />
+
+  if (authChecked && !authUser) return <Redirect to='/' />
 
   return <>{children}</>
 }
