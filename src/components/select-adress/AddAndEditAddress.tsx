4014ff866b565e0a3171b02d5c6fc16fc452c5d9
autsada <autsada.t@gmail.com>
2020-12-07 21:19:26 +0700

    S7 L98 Add New Address

--- src/components/select-adress/AddAndEditAddress.tsx
+++ src/components/select-adress/AddAndEditAddress.tsx
@@ -1,18 +1,90 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import Input from '../Input'
+import Button from '../Button'
+import { useAddShippingAddress } from '../../hooks/useAddShippingAddress'
+import { Address, UserInfo } from '../../types'
 
-interface Props {}
+interface Props {
+  userInfo: UserInfo | null
+}
 
-const AddAndEditAddress: React.FC<Props> = () => {
+const AddAndEditAddress: React.FC<Props> = ({ userInfo }) => {
+  const { addNewAddress, loading, error } = useAddShippingAddress()
+
+  const { register, errors, handleSubmit, reset } = useForm<
+    Omit<Address, 'index'>
+  >()
+
+  const handleAddNewAddress = handleSubmit(async (data) => {
+    if (!userInfo) return
+
+    const finished = await addNewAddress(data, userInfo)
+
+    if (finished) reset()
+  })
+
   return (
-    <form className='form'>
-      <Input label='Fullname' name='fullname' placeholder='Full name' />
-      <Input label='Address1' name='address1' placeholder='Adress 1' />
-      <Input label='Address2' name='address2' placeholder='Address 2' />
-      <Input label='City' name='city' placeholder='City' />
-      <Input label='Zipcode' name='zipcode' placeholder='Zip code' />
-      <Input label='Phone' name='phone' placeholder='Phone' />
+    <form
+      className='form'
+      onSubmit={handleAddNewAddress}
+      style={{ width: '100%' }}
+    >
+      <Input
+        label='Fullname'
+        name='fullname'
+        placeholder='Your full name'
+        ref={register({ required: 'Full name is required.' })}
+        error={errors.fullname?.message}
+      />
+      <Input
+        label='Address1'
+        name='address1'
+        placeholder='Street address, P.O. box, company name'
+        ref={register({
+          required: 'Street address, P.O. box, company name are required.',
+        })}
+        error={errors.address1?.message}
+      />
+      <Input
+        label='Address2'
+        name='address2'
+        placeholder='Apartment, suite, building, floor, etc.'
+      />
+      <Input
+        label='City'
+        name='city'
+        placeholder='City'
+        ref={register({
+          required: 'City is required.',
+        })}
+        error={errors.city?.message}
+      />
+      <Input
+        label='Zipcode'
+        name='zipCode'
+        placeholder='Zip code'
+        ref={register({
+          required: 'Zip code is required.',
+        })}
+        error={errors.zipCode?.message}
+      />
+      <Input
+        label='Phone'
+        name='phone'
+        placeholder='Phone'
+        ref={register({
+          required: 'Phone is required.',
+        })}
+        error={errors.phone?.message}
+      />
+
+      <Button width='100%' loading={loading} disabled={loading}>
+        Submit
+      </Button>
+
+      {error && <p className='paragraph paragraph--error'>{error}</p>}
     </form>
   )
 }
--- src/pages/SelectAddress.tsx
+++ src/pages/SelectAddress.tsx
@@ -15,16 +15,16 @@ const SelectAddress: React.FC<Props> = () => {
       <h2 className='header'>Select a shipping address</h2>
 
       <div className='select-address'>
-        {userInfo?.shippingAddresses?.length && (
-          <div className='select-address__existing'>
+        <div className='select-address__existing'>
+          {userInfo?.shippingAddresses?.length && (
             <div>Shipping addressed go here</div>
-          </div>
-        )}
+          )}
+        </div>
 
         <div className='select-address__add-new'>
           <h3 className='header'>Add a new address</h3>
 
-          <AddAndEditAddress />
+          <AddAndEditAddress userInfo={userInfo} />
         </div>
       </div>
     </div>
