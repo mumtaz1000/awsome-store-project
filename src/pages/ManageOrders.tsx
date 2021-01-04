41594a6de243b5c9a97052fcc0e8b905d7c30238
autsada <autsada.t@gmail.com>
2021-01-04 11:03:06 +0700

    S13 L170 Orders By Tab 2

--- src/pages/ManageOrders.tsx
+++ src/pages/ManageOrders.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Spinner from '../components/Spinner'
 import ManageOrderItem from '../components/manage-orders/ManageOrderItem'
+import Tab from '../components/Tab'
+import { orderTabType } from './Orders'
 import { useOrdersContext } from '../state/orders-context'
 import { useSearchContext } from '../state/search-context'
-import { Order } from '../types'
+import { useSelectTab } from '../hooks/useSelectTab'
+import { Order, OrderTab } from '../types'
+import { orderTabs } from '../helpers'
 
 interface Props {}
 
@@ -13,6 +17,25 @@ const ManageOrders: React.FC<Props> = () => {
     ordersState: { orders, loading, error },
   } = useOrdersContext()
   const { searchedItems } = useSearchContext()
+
+  const { activeTab } = useSelectTab<OrderTab>(orderTabType, 'New')
+
+  const [ordersByTab, setOrdersByTab] = useState(
+    orders ? orders.filter((order) => order.shipmentStatus === 'New') : null
+  )
+
+  useEffect(() => {
+    if (!orders) {
+      setOrdersByTab(null)
+      return
+    }
+
+    if (activeTab === 'All') setOrdersByTab(orders)
+    else
+      setOrdersByTab(
+        orders.filter((order) => order.shipmentStatus === activeTab)
+      )
+  }, [activeTab, orders, setOrdersByTab])
 
   if (loading) return <Spinner color='grey' height={50} width={50} />
 
@@ -26,7 +49,16 @@ const ManageOrders: React.FC<Props> = () => {
       <div className='orders-header'>
         <h2 className='header header--orders'>Your orders</h2>
 
-        <div className='orders-tabs'></div>
+        <div className='orders-tabs'>
+          {orderTabs.map((tab) => (
+            <Tab
+              key={tab}
+              label={tab}
+              activeTab={activeTab}
+              tabType={orderTabType}
+            />
+          ))}
+        </div>
       </div>
 
       <div className='orders-details'>
@@ -63,7 +95,8 @@ const ManageOrders: React.FC<Props> = () => {
             )}
           </>
         ) : (
-          orders.map((order) => (
+          ordersByTab &&
+          ordersByTab.map((order) => (
             <ManageOrderItem key={order.id} order={order} />
           ))
         )}
